bbd0877b8210c23765314055b2728daf34c544ac
Jeff Miccolis <jeff@miccolis.net>
2010-12-24 00:31:56 -0500

    Logic clean up and using local variables.

--- csv2json.bash
+++ csv2json.bash
@@ -2,6 +2,7 @@
 
 # assumptions
 # - no multi-line rows
+# - support escaped quotes ("") in cells.
 # - first line is a header row.
 # - column names are all simple (no spaces or commas, etc)
 # - first item on a row is key of the object.
@@ -13,7 +14,8 @@ _IFS=$IFS;
 # @param attribute row
 # @param index.
 function get_attribute {
-  INDEX=$2;
+  local INDEX=$2;
+
   IFS=',';
   for COL in $1; do
     if [ $INDEX == '0' ]; then
@@ -31,19 +33,24 @@ function get_attribute {
 # @param number of attributes
 # @param input to parse
 function parse_row {
+  local INDEX=0;
+  local ELEM='';
+
   IFS=',';
-  INDEX=0;
   for ELEM in $3; do
     if [ $INDEX == '0' ]; then
       echo -n "\"$ELEM\": {";
     else
-      # It's possible that we're in the midst of a cell that contained a
-      # comma
-      if [ "$PREVE" ]; then
-        ELEM="$PREVE,$ELEM";
-        unset PREVE;
+      # It's possible that we're:
+      # a) in the midst of a cell that contained a comma, or
+      # b) at the start of cell that begins with a comma.
+      if [ -n "$PREVE" -o ${ELEM:0:1} == '"' ]; then
+        # In case 'a'...
+        [ -n "$PREVE" ] && ELEM="$PREVE,$ELEM" && unset PREVE;
+        # In case 'b'...
+        [ ${ELEM:0:1} == '"' ] && ELEM=${ELEM:1};
+
         # Check for, and remove a closing quote here too.
-        # TODO besure the quote isn't escaped.
         if [ ${ELEM:$((${#ELEM}-1))} == '"' ]; then
           ELEM=${ELEM:0:$((${#ELEM}-1))};
         else
@@ -51,19 +58,8 @@ function parse_row {
           PREVE=$ELEM;
           continue;
         fi
-      # Check for, and remove, a leading double quote.
-      elif [ ${ELEM:0:1} == '"' ]; then
-        ELEM=${ELEM:1};
-        # Check for, and remove a closing quote.
-        if [ ${ELEM:$((${#ELEM}-1))} == '"' ]; then
-          ELEM=${ELEM:0:$((${#ELEM}-1))};
-        else
-          # If there is no closing double quote this cell isn't complete and we
-          # need the value of the next cell.
-          PREVE=$ELEM;
-          continue;
-        fi
       fi
+
       IFS=_IFS;
       echo -n "\"$(get_attribute $1 $INDEX)\": \"$ELEM\"";
       IFS=',';
@@ -82,8 +78,9 @@ function parse_row {
 # Validate the header and return (via `echo`) it's length.
 # - TODO real validation...
 function process_header {
+  local COLS=0
+
   IFS=',';
-  COLS=0
   for COL in $1; do
     COLS=$(($COLS+1));
   done;
@@ -94,8 +91,8 @@ function process_header {
 # Parse a file, line by line
 # @param filename to parse
 function parse_input {
-  ATTR='';
-  COMMA=0;
+  local ATTR='';
+  local COMMA=0;
 
   echo -n '{';
   IFS=$'\n';
