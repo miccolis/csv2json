b875bb307f5d87b891681e9070098edc007df7d5
Jeff Miccolis <jeff@miccolis.net>
2010-12-28 19:32:01 -0500

    Merging line and cell iterations in preparation for handling line breaks in cells.

--- csv2json.bash
+++ csv2json.bash
@@ -12,6 +12,19 @@
 # Hold on to the original IFS value.
 _IFS=$IFS;
 
+# Validate the header and return (via `echo`) it's length.
+# - TODO real validation...
+function process_header {
+  local COLS=0
+
+  IFS=',';
+  for COL in $1; do
+    COLS=$(($COLS+1));
+  done;
+  IFS=_IFS;
+  echo $COLS;
+}
+
 # Return (via `echo`) string name for the attribute
 # @param attribute row
 # @param index.
@@ -61,82 +74,20 @@ function escape_convert {
   return 0;
 }
 
-# Transform a comma seperated row into JSON. `echo`s the JSON string as it is
-# built.
-# @param attribute row
-# @param number of attributes
-# @param input to parse
-function parse_row {
-  local INDEX=0;
-  local ELEM='';
-
-  IFS=',';
-  for ELEM in $3; do
-    if [ $INDEX == '0' ]; then
-      echo -n "\"$ELEM\": {";
-    else
-      # It's possible that we're:
-      # a) in the midst of a quoted cell that contained a comma, or
-      # b) at the start of quoted cell.
-      if [ -n "$PREVE" -o ${ELEM:0:1} == '"' ]; then
-        # In case 'a'...
-        [ -n "$PREVE" ] && ELEM="$PREVE,$ELEM" && unset PREVE;
-        # In case 'b'...
-        [ ${ELEM:0:1} == '"' ] && ELEM=${ELEM:1};
-
-        # Check for, and remove a closing quote here too.
-        if [ ${ELEM:$((${#ELEM}-1))} == '"' ]; then
-          ELEM=${ELEM:0:$((${#ELEM}-1))};
-        else
-          # Don't find the closing double quote? Keep looking.
-          PREVE=$ELEM;
-          continue;
-        fi
-      fi
-
-      IFS=$'\n';
-      ELEM=$(escape_convert $ELEM)
-
-      IFS=' ';
-      echo -n "\"$(get_attribute $1 $INDEX)\": \"$ELEM\"";
-      IFS=',';
-
-      # If this isn't the last element add a comma.
-      if [ $INDEX -lt $(($2 - 1)) ]; then
-        echo -n ',';
-      fi;
-    fi
-    INDEX=$((INDEX+1));
-  done
-  echo -n "}";
-  IFS=_IFS;
-}
-
-# Validate the header and return (via `echo`) it's length.
-# - TODO real validation...
-function process_header {
-  local COLS=0
-
-  IFS=',';
-  for COL in $1; do
-    COLS=$(($COLS+1));
-  done;
-  IFS=_IFS;
-  echo $COLS;
-}
-
 # Parse a file, line by line
 # @param filename to parse
 function parse_input {
   local ATTR='';
-  COMMA=0;
+  local COMMA=0;
+  local ELEM='';
+  local INDEX=0;
 
   # Process header.
   read;
-  COLS=$(process_header $REPLY);
-  ATTR=$REPLY;
-
-  echo -n '{';
+  local COLS=$(process_header $REPLY);
+  local ATTR=$REPLY;
+  
+  # Read and process lines.
   IFS=$'\n';
   while read LINE; do
     if [ $COMMA == 1 ]; then
@@ -144,15 +95,61 @@ function parse_input {
     else
       COMMA=1;
     fi
-    echo -n $(parse_row $ATTR $COLS $LINE);
+
+    INDEX=0
+
+    IFS=',';
+    # Process elements within lines.
+    for ELEM in $LINE; do
+      if [ $INDEX == '0' ]; then
+        echo -n "\"$ELEM\": {";
+      else
+        # It's possible that we're:
+        # a) at the start of quoted cell.
+        # b) in the midst of a quoted cell that contained a comma, or
+        # c) in the midst of a quoted cell that contained a `\n`
+        IFS=$'\n';
+        if [ -n "$PREVE" -o ${ELEM:0:1} == '"' ]; then
+          # In case 'a'...
+          [ ${ELEM:0:1} == '"' ] && ELEM=${ELEM:1};
+          # In case 'b'...
+          [ -n "$PREVE" ] && ELEM="$PREVE,$ELEM" && unset PREVE;
+          # In case 'c'...
+
+          # Check for, and remove a closing quote here too.
+          # TODO we'll have a problem here with '""","'
+          if [ ${ELEM:$((${#ELEM}-1))} == '"' ]; then
+            ELEM=${ELEM:0:$((${#ELEM}-1))};
+          else
+            # Don't find the closing double quote? Keep looking.
+            PREVE=$ELEM;
+            continue;
+          fi
+        fi;
+
+        ## TODO not sure the best place to do this...
+        IFS=$'\n';
+        ELEM=$(escape_convert $ELEM)
+
+        IFS=' ';
+        echo -n "\"$(get_attribute $ATTR $INDEX)\": \"$ELEM\"";
+
+        # If this isn't the last element add a comma.
+        if [ $INDEX -lt $(($COL - 1)) ]; then
+          echo -n ',';
+        fi;
+      fi;
+      INDEX=$((INDEX+1));
+    done;
+    echo -n "}";
   done;
-  IFS=_IFS;
-  echo '}';
 }
 
 
 if [ -z $1 ]; then
+  echo -n '{';
   parse_input;
+  echo '}';
 elif [ -f $1 ]; then
   cat $1 | $0;
 else
