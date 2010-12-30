e62231e5d2b58f5e15e4c677c7d25dc8e9e0a8ea
Jeff Miccolis <jeff@miccolis.net>
2010-12-30 16:25:54 -0500

    Swapping out the main parser logic.

--- csv2json.bash
+++ csv2json.bash
@@ -145,10 +145,79 @@ function parse_input {
   done;
 }
 
+function convert_file {
+  local COUNT=0; # row counter
+  local INDEX=0; # current index.
+  local POS=0; # char index.
+  local SIMPLE=1; # `0` if current cell is quoted, `1` if it's simple.
+  local OUT; # Value of a cell.
+
+  # Process header.
+  read;
+  local COLS=$(process_header $REPLY);
+  local ATTR=$REPLY;
+  
+  # Read and process lines.
+  read && LINE=$REPLY;
+  while [ -n "$LINE" ]; do
+    [ $COUNT -gt 0 ] && echo -n ',';
+    echo -n '{';
+    while [ $INDEX -lt $COLS ]; do
+      # If this is a complex cell, unset the simple flag and strip the leading
+      # double quote.
+      if [ ${LINE:0:1} == '"' ]; then
+        SIMPLE=0;
+        LINE=${LINE:1};
+      fi
+      # Walk the line, striping off completed cells.
+      while [ $POS -lt ${#LINE} ]; do
+        if [ $SIMPLE == 1 ]; then
+          if [ ${LINE:$POS:1} == ',' ]; then
+            OUT=${LINE:0:$POS} && LINE=${LINE:$((POS+1))} && POS=0 && break;
+          elif [ $((POS+1)) == ${#LINE} -a $((INDEX+1)) == $COLS ]; then
+            # TODO error handling if a line is missing cells.
+            OUT=${LINE:0:$((POS+1))} && LINE=${LINE:$POS} && break;
+          fi
+        else
+          ## Two double quotes are an `escaped` quote, ignore them.
+          if [ $((POS+1)) -lt ${#LINE} -a ${LINE:$POS:2} == '""' ]; then
+            POS=$((POS+1));
+          elif [ "${LINE:$POS:1}" == '"' ];then
+            # TODO error handling if a cell is prematurely closed.
+            OUT=${LINE:0:$POS} && LINE=${LINE:$POS} && POS=0 && break;
+          fi
+          # If we don't have output and we're at the end of the line we've got
+          # a line break in the cell, so pull the next line in.
+          if [ -z "$OUT" -a $((POS+1)) == ${#LINE} ]; then
+            # TODO this newline creation is broken.
+            read && LINE="$LINE\n$REPLY";
+          fi
+        fi
+        POS=$((POS+1));
+      done;
+
+      IFS=$'\a'; # Split on the `bell`, they'll never use that!
+      OUT=$(escape_convert $OUT)
+
+      if [ $INDEX == 0 ]; then
+        echo -n "\"$OUT\": {";
+      else
+        IFS=' ';
+        echo -n "\"$(get_attribute $ATTR $INDEX)\": \"$OUT\"";
+      fi
+
+      unset OUT;
+      INDEX=$((INDEX+1));
+    done;
+    echo -n "}" && COUNT=$((COUNT+1));
+    unset LINE && read && LINE=$REPLY && POS=0 && INDEX=0;
+  done;
+}
+
 
 if [ -z $1 ]; then
   echo -n '{';
-  parse_input;
+  convert_file
   echo '}';
 elif [ -f $1 ]; then
   cat $1 | $0;
